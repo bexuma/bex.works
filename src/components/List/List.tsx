0b53721ed008bf2c29db590976c336c667852a2e
Bexultan Myrzatay <bexultan.dev@gmail.com>
2025-08-07 15:12:54 +0500

    refactor(List): wrap under one element

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -5,17 +5,17 @@ export const List = ({ list }: { list: ListItemType[] | string[] }) => {
 
   return (
     <ul className="space-y-1">
-      {list.map((item, index) =>
-        typeof item === 'string' ? (
-          <li key={index} className="before:mr-1 before:content-['–']">
-            {item}
-          </li>
-        ) : (
-          <li key={index} className="before:mr-1 before:content-['–']">
-            <span className="font-semibold">{item.heading}</span>: {item.body}
-          </li>
-        ),
-      )}
+      {list.map((item, index) => (
+        <li key={index} className="before:mr-1 before:content-['–']">
+          {typeof item === 'string' ? (
+            item
+          ) : (
+            <>
+              <span className="font-semibold">{item.heading}</span>: {item.body}
+            </>
+          )}
+        </li>
+      ))}
     </ul>
   )
 }
