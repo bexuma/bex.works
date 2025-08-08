a552f1527ed5f3471c8e0a7e9662d8e226596aee
Bexultan Myrzatay <bexultan.dev@gmail.com>
2025-08-08 16:29:59 +0500

    fix(Navbar/ThemeToggle): add mount listener

--- src/components/Navbar/ThemeToggle.tsx
+++ src/components/Navbar/ThemeToggle.tsx
@@ -1,8 +1,16 @@
 'use client'
+import { useEffect, useState } from 'react'
 import useTheme from './useTheme'
 
 export default function ThemeToggle() {
   const { theme, toggleTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
 
   return (
     <button
@@ -21,7 +29,7 @@ export default function ThemeToggle() {
             strokeLinecap="round"
             strokeLinejoin="round"
             d="M8 1.906V.97m4.309 2.722.663-.663M14.094 8h.937m-2.722 4.309.663.663M8 14.094v.937M3.691 12.31l-.663.663M1.906 8H.97M3.69 3.691l-.663-.663M3.781 8a4.219 4.219 0 1 0 8.438 0A4.219 4.219 0 0 0 3.78 8"
-          ></path>
+          />
         </svg>
       ) : (
         <svg
@@ -35,7 +43,7 @@ export default function ThemeToggle() {
             strokeLinecap="round"
             strokeLinejoin="round"
             d="M7.99 2.844h2.813M9.397 1.438V4.25m2.812 1.406h1.875m-.937-.937v1.875m-1.875 3.75a6.096 6.096 0 0 1-5.828-7.875 6.091 6.091 0 1 0 8.375 7.313 6.06 6.06 0 0 1-2.547.562"
-          ></path>
+          />
         </svg>
       )}
     </button>
