c47adea3be7bfc384e9e44ce36008bff2383059a
Bexultan Myrzatay <bexultan.dev@gmail.com>
2025-08-08 16:43:17 +0500

    refactor(public): rename card.pdf

--- src/app/sqko/components/cards/DocumentCard/DocumentCard.tsx
+++ src/app/sqko/components/cards/DocumentCard/DocumentCard.tsx
@@ -8,8 +8,7 @@ export const DocumentCard = () => (
     </p>
     <iframe
       title="card"
-      src="/card.pdf"
-      // type="application/pdf"
+      src="/digital-document.pdf"
       className="border-light-300 dark:border-dark-300 h-[450px] w-full min-w-[290px] rounded-lg border sm:h-[450px] md:h-[570px] xl:h-[520px] 2xl:h-[570px]"
     ></iframe>
   </Card>
