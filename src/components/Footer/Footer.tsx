dc51e9be47d5563240c6839578d637945506090c
Bexultan Myrzatay <bexultan.dev@gmail.com>
2025-08-11 16:45:51 +0500

    fix(Footer): apply text classnames

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,9 @@
 export const Footer = () => (
   <footer className="px-4">
     <div className="text-light-400 dark:text-dark-400 container mx-auto flex h-14 items-center justify-center sm:h-16">
-      <span>© 2025 Bexultan Myrzatay</span>
+      <span className="text-light-500 dark:text-dark-500 text-sm">
+        © 2025 Bexultan Myrzatay
+      </span>
     </div>
   </footer>
 )
