a9da04c34439dbde995897e4858def55e8f4b4a7
Bexultan Myrzatay <bexultan.dev@gmail.com>
2025-08-12 11:11:32 +0500

    fix(/sqko/HistorySection): layout on md screens

--- src/app/sqko/sections/HistorySection/HistorySection.tsx
+++ src/app/sqko/sections/HistorySection/HistorySection.tsx
@@ -8,17 +8,17 @@ import { Section } from '../../components'
 
 export const HistorySection = () => (
   <Section heading="History">
-    <div className="flex flex-col gap-2 sm:gap-4 md:grid md:grid-cols-12 md:grid-rows-37 lg:grid-rows-9 xl:grid-rows-none">
-      <div className="md:col-span-7 md:row-span-23 lg:col-span-8 lg:row-span-6 xl:col-span-6 xl:row-span-2 2xl:row-span-2">
+    <div className="flex flex-col gap-2 sm:gap-4 md:grid md:grid-cols-12 md:grid-rows-36 lg:grid-rows-9 xl:grid-rows-none">
+      <div className="md:col-span-7 md:row-span-22 lg:col-span-8 lg:row-span-6 xl:col-span-6 xl:row-span-2 2xl:row-span-2">
         <UniversityCard />
       </div>
-      <div className="md:col-span-5 md:row-span-20 lg:col-span-4 lg:row-span-5 xl:col-span-6 xl:row-span-1 2xl:col-start-7 2xl:row-span-1">
+      <div className="md:col-span-5 md:row-span-19 lg:col-span-4 lg:row-span-5 xl:col-span-6 xl:row-span-1 2xl:col-start-7 2xl:row-span-1">
         <TransitionCard />
       </div>
-      <div className="md:col-span-7 md:row-span-14 md:row-start-24 lg:col-span-8 lg:row-span-3 lg:row-start-7 xl:col-span-6 xl:row-span-1 2xl:col-span-3 2xl:col-start-7 2xl:row-span-1 2xl:row-start-2">
+      <div className="md:col-span-7 md:row-span-14 md:row-start-23 lg:col-span-8 lg:row-span-3 lg:row-start-7 xl:col-span-6 xl:row-span-1 2xl:col-span-3 2xl:col-start-7 2xl:row-span-1 2xl:row-start-2">
         <FailureCard />
       </div>
-      <div className="md:col-span-5 md:row-span-17 md:row-start-21 lg:col-span-4 lg:col-start-9 lg:row-span-4 lg:row-start-6 xl:col-span-12 xl:row-span-1 2xl:col-span-3 2xl:col-start-10 2xl:row-span-1 2xl:row-start-2">
+      <div className="md:col-span-5 md:row-span-17 md:row-start-20 lg:col-span-4 lg:col-start-9 lg:row-span-4 lg:row-start-6 xl:col-span-12 xl:row-span-1 2xl:col-span-3 2xl:col-start-10 2xl:row-span-1 2xl:row-start-2">
         <CareerCard />
       </div>
     </div>
--- src/app/sqko/sections/HistorySection/responsive.md
+++ src/app/sqko/sections/HistorySection/responsive.md
@@ -12,8 +12,8 @@ sm (12/12)
 |                Career               |
 
 md (7/12, 5/12)
-|       University       | Transition | 670 590 | 67 59 | 69 60 | 23 20
-|        Failure         |   Career   | 410 510 | 41 51 | 42 51 | 14 17
+|       University       | Transition | 670 590 | 67 59 | 69 60 | 23 20 | 22 19
+|        Failure         |   Career   | 410 510 | 41 51 | 42 51 | 14 17 | 14 17
                                         +20       +2
 lg (8/12, 4/12)
 |       University       | Transition | 6 5
